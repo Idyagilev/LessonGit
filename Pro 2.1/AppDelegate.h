b5022157f7d95d16960f49b65048dbc40cebb488
Idyagilev <T@MacBook-Pro-T.local>
2016-03-09 19:09:12 -0800

    first comit

---
+++ Pro 2.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pro 2.1
+//
+//  Created by T on 09.03.16.
+//  Copyright © 2016 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pro 2.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pro 2.1
+//
+//  Created by T on 09.03.16.
+//  Copyright © 2016 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
